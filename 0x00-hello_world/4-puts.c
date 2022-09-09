49e648f59b9b5cccc2fd11a1512f1147a02d17c0
Thomas-A1 <thomasquarshie36@gmail.com>
2022-09-09 14:38:40 -0700

    A C file that prints out a string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
-/*
-* The main function prints out:
-* "Programming is like building a multilingual puzzle"
-* The function returns 0
+/**
+* The main function prints out a string
+* The main function returns 0
 */
 int main(void){
 	puts("\"Programming is like building a multilingual puzzle");

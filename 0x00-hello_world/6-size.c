707c127193e6c6bbb7b5b6bcf15fb68a94961d03
Thomas-A1 <thomasquarshie36@gmail.com>
2022-09-09 15:31:31 -0700

    A C file that prints out the size of data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,22 +12,24 @@ int main(void)
 
 	int a;
 
-	long long int c;
+	long long int ll;
 
-	char d;
+	char c;
 
 	float f;
 
 	long int b;
 
-	printf("Size of a char: %lu byte(s)\n", sizeof(d));
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));
 
 	printf("Size of an int: %lu byte(s)\n", sizeof(a));
 
 	printf("Size of a long int: %lu byte(s)\n", sizeof(b));
 
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
 
 	printf("Size of a float: %lu byte(s)\n", sizeof(f));
 
 	return (0);
+
+}

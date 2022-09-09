f4e2495e1a4dbdb5bc2f2e0ee46f93d1e1b24eac
Thomas-A1 <thomasquarshie36@gmail.com>
2022-09-09 14:50:08 -0700

    A C script that prints a string

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * Main function: Prints out a string
+ * Returns: 0
+ *
+*/
+
+int main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
+

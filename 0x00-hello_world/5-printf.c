d8f499ede3d951c9e34fa6ae0bb9a0cd01147a50
Thomas-A1 <thomasquarshie36@gmail.com>
2022-09-09 14:52:42 -0700

    A C script that prints a string

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,8 +8,16 @@
 
 int main(void)
 
+
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+
+
+			printf("with proper grammar, but the outcome is a piece of art,\n");
+
+
+
+					return (0);
+
+
 }
 

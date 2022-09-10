7b693c9eabedfa06d8bcbb490b4ad32012f5f203
Thomas-A1 <thomasquarshie36@gmail.com>
2022-09-10 13:31:41 -0700

    Printing lowercase and uppercase characters

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,18 +3,19 @@
 /**
  * main - Entry of the program
  * Printing both lowercase and uppercase characters
- * Return : 0
+ * Return:0
  */
 
 int main(void)
 {
 	char character;
+	char Character;
+
 	for (character = 'a'; character <= 'z'; character++)
 		putchar(character);
-	putchar('\n');
 	
-	for (character = 'A'; character <= 'Z'; character++)
-		putchar(character);
+	for (Character = 'A'; Character <= 'Z'; Character++)
+		putchar(Character);
 	putchar('\n');
 	return (0);
 

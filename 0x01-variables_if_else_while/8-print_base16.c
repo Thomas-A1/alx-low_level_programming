1f06ceea41f129714ba71aa024e6f437c0ae6b81
Thomas-A1 <thomasquarshie36@gmail.com>
2022-09-10 14:13:39 -0700

    Printing all the numbers of base 16 in lowercase

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -14,13 +14,13 @@ char hex = '0';
 char a_to_f = 'a';
 while (hex <= '9')
 {
-	putchar(hex);
-	hex++;	
+putchar(hex);
+hex++;	
 }
 while (a_to_f <= 'f')
 {
-	putchar(a_to_f);
-	a_to_f++;										
+putchar(a_to_f);
+a_to_f++;
 }
 putchar('\n');
 return (0);

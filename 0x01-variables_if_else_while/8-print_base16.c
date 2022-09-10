5d08749e048975dce573b4d34bdba2f39f7357d1
Thomas-A1 <thomasquarshie36@gmail.com>
2022-09-10 14:56:12 -0700

    Printing numbers of base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,25 +3,24 @@
 /**
  * main - entry block
  * Description: Printingnumbers of base 16 in lowercase
- * Return:0 
+ * Return:0
  */
-
 
 int main(void)
 {
-	int h = '0';
+	int a = '0';
 	int a_to_f = 'a';
 
-	while (h <= '9')		
+	while (a <= '9')
 	{
-		putchar(h);
-		h++;
+		putchar(a);
+		a++;
 	}
 	while (a_to_f <= 'f')
 	{
 		putchar(a_to_f);
 		a_to_f++;
 	}
-	putchar('\n');	
+	putchar('\n');
 	return (0);
 }

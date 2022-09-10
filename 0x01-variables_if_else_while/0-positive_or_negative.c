766d7bcbbc32b8051c513c9b90a38a4ec372acef
Thomas-A1 <thomasquarshie36@gmail.com>
2022-09-10 12:36:12 -0700

    Using if and else statements to tell the state of a number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,8 +15,6 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-/* your code goes there */
-return (0);
 
 if (n > 0)
 	printf("%d is positive\n", n);
@@ -26,5 +24,6 @@ else if (n == 0)
 
 else
 	printf("%d is negative", n);
+return (0);
 }
 

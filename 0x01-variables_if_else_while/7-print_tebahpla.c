eb3ba3e5b9c42a3f2e9cb7a37fb740a435ef8625
Thomas-A1 <thomasquarshie36@gmail.com>
2022-09-10 14:00:42 -0700

    Printing lowercase characters in reverse form

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -Entry of the program
+ * Printing lowercase characters in reverse
+ * Return:0
+ */
+
+int main(void)
+
+{
+char character;
+for (character = 'z' ; character >= 'a'; character--)
+	putchar(character);
+putchar('\n');
+return (0);
+}

f5dff4b3d6035cdc3b0e304895899debfe7106de
Thomas-A1 <thomasquarshie36@gmail.com>
2022-09-13 12:42:31 -0700

    Characters in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * Main: Entry program
+ * Alphabet in lowercase
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char character;
+	for (character = 'a'; character <= 'z'; c++)
+		_putchar(character);
+	_putchar('\n');
+}

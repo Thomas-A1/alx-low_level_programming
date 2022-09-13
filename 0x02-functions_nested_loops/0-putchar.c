0bf61395831d77004f5f4566a7af87b07db76d66
Thomas-A1 <thomasquarshie36@gmail.com>
2022-09-13 12:32:17 -0700

    Printing the characters in a word(0._putchar)

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Printing putchar
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char[] = "_putchar";
+	int character;
+
+	for (character = 0; character < sizeof(char); character++)
+		_putchar(char[character]);
+	_putchar('\n');
+
+	return (0);
+}

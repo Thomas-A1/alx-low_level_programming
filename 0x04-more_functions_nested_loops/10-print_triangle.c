491979da91c07f1ec7df3cd6acbbb7766fe23aa9
Thomas-A1 <thomasquarshie36@gmail.com>
2022-09-15 15:29:22 -0700

    Formualting a triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,21 +8,22 @@
  */
 
 void print_triangle(int size)
-
 {
-	int i, j;
+int i, j;
 
-	if (size > 0)
+if (size > 0)
+{
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
+		for (j = 0; j < size; j++)
 		{
-			for (j = 0; j < size; j++)
+			if ((size - i - 1) > j)
 			{
-				if ((size - i - 1) > j)
-
-					_putchar(' ');
-				else
-					_putchar('#');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('#');
 			}
 			_putchar('\n');
 		}

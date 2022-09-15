e3d5c72e0f44765ba99f0f0b8caaecc309fced1e
Thomas-A1 <thomasquarshie36@gmail.com>
2022-09-15 15:15:28 -0700

    Formulating a triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -4,37 +4,63 @@
 /**
  * main - prints Fizz for int divisible by 3 or Buzzz divisible by 5
  *
- * Return: void
+ * Return: int
  */
 
 int main(void)
+
 {
+
 	int i;
 
+
+
 	for (i = 1; i <= 100; i++)
+
 	{
+
 		if (i % 3 == 0 && i % 5 != 0)
+
 		{
+
 			printf("Fizz");
+
 		}
+
 		else if (i % 3 != 0 && i % 5 == 0)
+
 		{
+
 			printf("Buzz");
+
 		}
+
 		else if (i % 3 == 0 && i % 5 == 0)
+
 		{
+
 			printf("FizzBuzz");
+
 		}
+
 		else
+
 		{
+
 			printf("%d", i);
+
 		}
+
 		if (i != 100)
-		{
+
 			putchar(' ');
-		}
+
 		else
+
 			putchar('\n');
+
 	}
+
 	return (0);
+
 }

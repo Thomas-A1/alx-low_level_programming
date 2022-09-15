bc27482bfd4f0a329a283ddc276956ed5bba1894
Thomas-A1 <thomasquarshie36@gmail.com>
2022-09-15 15:35:01 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,29 +13,29 @@ int num = 1;
 
 while (num++ < 100)
 {
-	if ((num % 3 == 0) && (num % 5 == 0))
+if ((num % 3 == 0) && (num % 5 == 0))
+{
+	printf("FizzBuzz ");
+}
+else if ((num % 3) == 0)
+{
+	printf("Fizz ");
+}
+else if ((num % 5) == 0)
+{
+	if (num != 100)
 	{
-		printf("FizzBuzz ");
-	}
-	else if ((num % 3) == 0)
-	{
-		printf("Fizz ");
-	}
-	else if ((num % 5) == 0)
-	{
-		if (num != 100)
-		{
-			printf("Buzz ");
-		}
-		else
-		{
-			printf("Buzz");
-		}
+		printf("Buzz ");
 	}
 	else
 	{
-		printf("%d ", num);
+		printf("Buzz");
 	}
+}
+else
+{
+	printf("%d ", num);
+}
 }
 printf("\n");
 return (0);

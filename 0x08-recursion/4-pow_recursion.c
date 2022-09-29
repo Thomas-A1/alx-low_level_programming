cb1678e3dfde8fffa97528f7a73548d941af2249
Thomas-A1 <thomasquarshie36@gmail.com>
2022-09-28 19:47:46 -0700

    Performing the exponent of a number

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the value of x to the power y
+ * @x: parameter passed as the base
+ * @y: parameter passed as the exponent
+ * Return: the exponent of x
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}

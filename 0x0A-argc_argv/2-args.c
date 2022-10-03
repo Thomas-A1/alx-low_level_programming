aab364fdd65b152b0740bdcc1d39398b0c7c1df5
Thomas-A1 <thomasquarshie36@gmail.com>
2022-10-03 10:07:13 -0700

    Printing all arguments

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,13 +5,14 @@
  * @argc: Number of arguments passed to the function
  * @argv: argument vector of pointers to strings
  */
+
 int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
 	{
-		printf("%s\n", argv[i]);
+		printf ("%s\n", argv[i]);
 	}
 	return (0);
 i}

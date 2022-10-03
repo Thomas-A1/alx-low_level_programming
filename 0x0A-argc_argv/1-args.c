cc1ff13fe1df24c2474f534604bdb468f08b32d2
Thomas-A1 <thomasquarshie36@gmail.com>
2022-10-03 10:13:34 -0700

    Printing the number of arguments passed

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,8 +2,10 @@
 
 /**
  * main - Prints the number of arguments passed to it
+ *
  * @argv: Number of arguments passed to the function
  * @argv: argument vector of pointers to strings
+ *
  * Return: 0
  */
 

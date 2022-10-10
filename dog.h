f21606555b2d6d00fc7edbc8f91b66310b19b40a
Thomas-A1 <thomasquarshie36@gmail.com>
2022-10-10 14:30:00 -0700

    Defining a new type

---
+++ dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Struct dog function
+ * @name: name as a variable as char type
+ * @age: age as a variable astype float
+ * @owner: owner as a variable of char type
+ * Description: A struct dog function
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
+void init_dog(struct dog *dt, char *name, float age, char *owner);void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /*DOG_H*/

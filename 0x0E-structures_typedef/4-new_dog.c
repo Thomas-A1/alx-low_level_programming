107da23fc8ddbc6ad490f20345904358f4f6125b
Thomas-A1 <thomasquarshie36@gmail.com>
2022-10-10 17:50:42 -0700

    Creating a new Struct dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,9 +10,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	int i, lenN, lenO;
-
 	struct dog *n_dog = NULL;
-	
 	lenN = 0;
 	
 	while (name[lenN] != '\0')
@@ -20,6 +18,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	lenO = 0;
 	while (owner[lenO] != '\0')
 		lenO++;
+	
 	n_dog = malloc(sizeof(struct dog));
 	if (n_dog == NULL)
 	{

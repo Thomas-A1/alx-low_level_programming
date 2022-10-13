42b197dc0b141081bf85cd38cfc247213ad53cb4
Thomas-A1 <thomasquarshie36@gmail.com>
2022-10-12 17:41:22 -0700

    Returning a pointer to a newly allocated space

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -4,20 +4,55 @@
  * @str: string to be copied
  * Return: NULL if fail
  */
-char *strdup(char *str)
+char *strlen(char *str)
 {
-	char *cp;
-	int i, len;
+	unsigned int i;
+	
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * _strcpy - Copies arrays
+ * @src: Array of elements
+ * @dest: destination array
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
 
-	if (str == NULL)
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/**
+ * _strdup -Array to print a string
+ * @str: array of elements
+ * Return: pointer
+ */
+char *_strdup(char *str)
+{
+	char *dst;
+	unsigned int size;
+	
+	if (str == 0)
+	{
 		return (NULL);
-	for (i = 0; str[i]; i++)
-		len++;
-	cp = malloc(sizeof(char) * (len + 1));
-	if (cp == NULL)
+	}
+	size = _strlen(str) + 1;
+	dst = (char *) malloc(size * sizeof(char));
+	if (dst == 0)
+	{
 		return (NULL);
-	for (i = 0; str[i]; i++)
-		cp[i] = str[i];
-	cp[len] = '\0';
-	return (cp);
+	}
+	_strcpy(dst, str);
+	return (dst);
 }

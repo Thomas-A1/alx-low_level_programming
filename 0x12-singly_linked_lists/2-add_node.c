b25b1db8cde893f90ebaad5b21cf8e12db59e3a6
Thomas-A1 <thomasquarshie36@gmail.com>
2022-10-21 16:28:54 -0700

    Adding a node to the beginning of a linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,12 +16,11 @@ list_t *add_node(list_t **head, const char *str)
 
 	if (!(head && str))
 		return (NULL);
-	new malloc(sizeof(list_t));
+	new_node = malloc(sizeof(list_t));
 	if (!new_node)
 		return (NULL);
 	for (i = 0; str[i]; i++)
-	{
-	}
+	{}
 	new_node->str = strdup(str);
 	if (!(new_node->str))
 	{
@@ -31,5 +30,6 @@ list_t *add_node(list_t **head, const char *str)
 	new_node->len = i;
 	new_node->next = *head;
 	*head = new_node;
+	
 	return (new_node);
 }

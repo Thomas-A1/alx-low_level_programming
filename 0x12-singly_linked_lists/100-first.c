dabe8593550b34223079a4af0b9eb64314dc8e44
Thomas-A1 <thomasquarshie36@gmail.com>
2022-10-21 16:53:30 -0700

    A function that prints before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * before_main - Prints before main
+ *
+ *
+ * Return: void
+ */
+void __attribute__((constructor)) before_main()
+{
+	printf("Tough to code in C\n");
+	printf("Yet I will not quit\n");
+}

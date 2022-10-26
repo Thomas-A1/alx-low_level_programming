2cad76999ee046b55cc6712ac597004d02b3f5f9
Thomas-A1 <thomasquarshie36@gmail.com>
2022-10-26 12:35:14 -0700

    Printing the number of elements in a linked list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -5,7 +5,7 @@
  * @h: the head of a linked list
  * Return: the number of elements
  */
-size listint_len(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	const listint_t *cursor = h;
 	size_t counter = 0;

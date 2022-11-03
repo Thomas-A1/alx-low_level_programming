d29fe548550701e310b678c56c1ae90179a2a9ab
Thomas-A1 <thomasquarshie36@gmail.com>
2022-11-03 14:38:57 -0700

    read a file and print the content to stdout

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,9 @@
 #include "main.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
 /**
  * read_textfile - reads a text file and prints the letters in it.
@@ -12,20 +17,27 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t nrd, nwr;
 	char *buf;
 
-	if (!filename)
+	if (filename == NULL)
 		return (0);
-	
-	fd = open(filename, o_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	
 	buf = malloc(sizeof(char) * (letters));
-	if (!buf)
+	if (buf == NULL)
+	{
+		close(fd);
 		return (0);
-	
+	}
 	nrd = read(fd, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
 	close(fd);
+	if (nrd == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	nwr = write(STDOUT_FILENO, buf, nrd);
 	free(buf);
+	if (nrd != nwr)
+		return (0);
 	return (nwr);
 }
